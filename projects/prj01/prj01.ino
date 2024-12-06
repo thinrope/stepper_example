3f7bb0991765576d502d2f75ae7ec6e11778295a
Kalin KOZHUHAROV <me.kalin@gmail.com>
2024-12-06 15:42:22 +0100

    deadend
    Done experimenting, stepper does not move, PSU tries to draw too much current with or without stepper (up to 3A). Afraid not to burn something...

--- projects/prj01/prj01.ino
+++ projects/prj01/prj01.ino
@@ -1,34 +1,46 @@
 
 //driver_signal Teensy_pin 
 //      VDD     3.3V
-#define MS1     2
-#define MS2     3
-#define MS3     4
-#define SLEEP   5
-#define STEP    6
-#define DIR     7
-#define ENABLE  8
-#define RESET   9
+#define MS1     21
+#define MS2     20
+#define MS3     19
+#define SLEEP   18
+#define STEP    17
+#define DIR     16
+#define ENABLE  15
+#define RESET   14
 //      GND     GND
 
 void setup()
 {
+	pinMode(LED_BUILTIN, OUTPUT);
+  for (int i=0; i < 3; i++) // blink 3 times
+  {
+    digitalWrite(LED_BUILTIN, HIGH); delay(50); digitalWrite(LED_BUILTIN, LOW); delay(150);
+  }
+  digitalWrite(LED_BUILTIN, HIGH); delay(1000); digitalWrite(LED_BUILTIN, LOW); //wait 1s before initializing
+
 	pinMode(MS1,	OUTPUT); digitalWrite(MS1,	LOW);
 	pinMode(MS2,	OUTPUT); digitalWrite(MS2,	LOW);
 	pinMode(MS3,	OUTPUT); digitalWrite(MS3,	LOW);
-	pinMode(SLEEP,	OUTPUT); digitalWrite(SLEEP,	HIGH);
+	pinMode(SLEEP,	OUTPUT); digitalWrite(SLEEP,	LOW);
 	pinMode(STEP,	OUTPUT); digitalWrite(STEP,	LOW);
 	pinMode(DIR,	OUTPUT); digitalWrite(DIR,	LOW);
 	pinMode(ENABLE,	OUTPUT); digitalWrite(ENABLE,	LOW);
 	pinMode(RESET,	OUTPUT); digitalWrite(RESET,	HIGH);
 	
-	//pinMode(13, OUTPUT);      digitalWrite(13, LOW);
+  for (int i=0; i < 3; i++) // blink 3 times slowly
+  {
+    digitalWrite(LED_BUILTIN, HIGH); delay(200); digitalWrite(LED_BUILTIN, LOW); delay(800);
+  }
+  digitalWrite(LED_BUILTIN, HIGH); delay(2000); digitalWrite(LED_BUILTIN, LOW); //wait 2s before starting the loop
+  digitalWrite(SLEEP, HIGH); delay(2);
 }
 
 void loop()
 {
 	digitalWrite(STEP, HIGH);
-	delayMicroseconds(2);   // 1 us by dataasheet
+	delayMicroseconds(10);   // 1 us by dataasheet
 	digitalWrite(STEP, LOW);
-	delay(995); // TBC
+	delay(999); // TBC
 }
