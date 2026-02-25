62f56c667dd1c6c64ffd5ae6bf9ad18900473dcd
Kalin KOZHUHAROV <me.kalin@gmail.com>
2026-02-25 22:04:24 +0100

    final code

---
+++ projects/prj03/prj03.ino
@@ -0,0 +1,107 @@
+//driver_signal MCU_pin 
+//      VDD     3.3V
+#define STEP    8
+#define DIR     9
+//      GND     GND
+#define BTN1    12
+// LED_BUILTIN  13
+
+// all in us; 400 pulses/rotation in full-step mode
+//
+//     pulse | time/rotation
+// min. 2000 | 0.8s
+//      2500 | 1.0s
+// max  5000 | 2.0s 
+long int pulse = 3750;  // time for one pulse (pulse+pause)
+
+long int ppulse = 100;  // pulse width, 100us is enough
+long int npulse = pulse - ppulse; // waittime till whole pulse is finished
+int direction = LOW;  // CCW
+
+boolean btn1_prev_state = LOW;
+int experiment_no = 0;
+
+void setup()
+{
+	Serial.begin(115200);
+  Serial.println("\n\n\n\n\n\n===============================================");
+  
+  pinMode(BTN1, INPUT);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+	pinMode(STEP,	OUTPUT); digitalWrite(STEP,	LOW);
+	pinMode(DIR,	OUTPUT); digitalWrite(DIR,	direction);
+	
+  Serial.printf("Setup completed. Using %d/%d (in us) pulses.\n", ppulse, npulse);
+}
+
+void loop()
+{
+  boolean btn1_state = digitalRead(BTN1);
+  if (btn1_state == HIGH && btn1_prev_state == LOW)
+  {
+    delay (100);
+    Serial.println("NOTE: BTN1 pressed!");
+    ignition();
+  }
+  btn1_prev_state = btn1_state;
+}
+
+void ignition()
+{
+  experiment_no++;
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  Serial.printf("===|experiement %02d|===\n", experiment_no);
+
+  digitalWrite(DIR, direction); delayMicroseconds(npulse);
+
+  Serial.printf("\tExperiment %02d, step1: lighting\n", experiment_no);
+  for (int i=0; i<20; i++)
+  {
+    digitalWrite(STEP, HIGH); delayMicroseconds(ppulse);
+	  digitalWrite(STEP, LOW);  delayMicroseconds(npulse);
+  }
+
+  Serial.printf("\tExperiment %02d, step2: moving to the side\n", experiment_no);
+  for (int i=0; i<30; i++)
+  {
+    digitalWrite(STEP, HIGH); delayMicroseconds(ppulse);
+	  digitalWrite(STEP, LOW);  delayMicroseconds(npulse*2);
+  }
+
+  Serial.printf("\tExperiment %02d, step3: burning\n", experiment_no);
+  delay(2000); // ms
+
+  Serial.printf("\tExperiment %02d, step4: down into the water\n", experiment_no);
+	for (int i=0; i<65; i++)
+  {
+    digitalWrite(STEP, HIGH); delayMicroseconds(ppulse);
+	  digitalWrite(STEP, LOW);  delayMicroseconds(npulse*2);
+  }
+  delay(2000); // ms
+
+  Serial.printf("\tExperiment %02d, step5: out of the water\n", experiment_no);
+	for (int i=0; i<85; i++)
+  {
+    digitalWrite(STEP, HIGH); delayMicroseconds(ppulse);
+	  digitalWrite(STEP, LOW);  delayMicroseconds(npulse*2);
+  }
+  Serial.printf("===|experiement %02d finished|===\n", experiment_no);
+  Serial.println("Change the match, position the arrow and press the button!");
+  blink_fast(5);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void blink_fast(int times)
+{
+  digitalWrite(LED_BUILTIN, LOW);
+  for (int i=0; i<times; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH); delay(100);
+    digitalWrite(LED_BUILTIN, LOW); delay(100);
+  }
+}
+
